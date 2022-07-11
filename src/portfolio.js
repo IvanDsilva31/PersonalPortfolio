3f8020342a8894fb0bd831f8cb94a9ee6d9d17cb
IvanDsilva31 <ivandsilva5@gmail.com>
2022-07-11 19:32:31 +0530

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -377,7 +377,7 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+    "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships.",
   header_image_path: "experience.svg",
   sections: [
     {
